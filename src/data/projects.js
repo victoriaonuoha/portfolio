57136de5833e5351f5315b58c3de88b304c969da
victoriaonuoha <onuohavictoria27august@gmail.com>
2026-03-30 14:50:10 +0100

    h

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,8 +3,16 @@
 import { g } from "framer-motion/client";
 
 const projects = [
-  {
+   {
     id: 1,
+    title: "Interactive E-Commerce Product Page",
+    image: "/dev-ecommerce.png",
+    tech: ["React", "Tailwind CSS", "Framer Motion", "Context API"],
+    github: "https://github.com/victoriaonuoha/devtoria_ecommerce",
+    live: "https://devtoria-ecommerce.vercel.app/"
+  },
+  {
+    id: 2,
     title: "URL Shortener",
     image: "/url_shortener_pic.png",
     tech: ["React", "Tailwind", "API"],
@@ -12,7 +20,7 @@ const projects = [
     live: "https://devtoria-url-shortener.vercel.app/"
   },
   {
-    id: 2,
+    id: 3,
     title: "Weather App",
     image: "/weather_short_pic.png",
     tech: ["React", "Real world API", "Tailwind"],
@@ -20,7 +28,7 @@ const projects = [
     live: "https://devtoria-weather-app.vercel.app/"
   },
   {
-    id: 3,
+    id: 4,
     title: "CredScore: A Credit Score Generator",
     image: "/credscore.png",
     tech: ["NextJs", "Tailwind"],
